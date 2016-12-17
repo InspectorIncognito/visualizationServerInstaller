4c7a83c0bd3f5de1010cc1ed8867fa6a4f8bacea
Matías Pavez <sebastian.ormeno@live.com>
2016-12-16 22:23:46 -0300

    fix error in thar $PROJECT_DEST

--- installScript.sh
+++ installScript.sh
@@ -227,12 +227,12 @@ if $project_configuration; then
 
   LAST_MIGRATION=$(sudo -u postgres -i psql -d "$DATABASE_NAME" -c "select name from django_migrations where app='AndroidRequests' ORDER BY applied DESC limit 1;"| sed '3q;d')
 
-  tar -zxvf "f$MIGRATION" "$PROJECT_DEST"/AndroidRequests/migrations 
-  sed -i -e 's/CHANGE_ME/"$LAST_MIGRATION"/g' "$PROJECT_DEST"/AndroidRequests/migrations/0011_auto_20161025_1616.py
+  tar -zxvf "f$MIGRATION" "$PROJECT_DEST"visualization/AndroidRequests/migrations 
+  sed -i -e 's/CHANGE_ME/"$LAST_MIGRATION"/g' "$PROJECT_DEST"visualization/AndroidRequests/migrations/0011_auto_20161025_1616.py
 
   # uptade the model of the database
-  python "$PROJECT_DEST"/manage.py migrate
-  python "$PROJECT_DEST"/manage.py collectstatic
+  python "$PROJECT_DEST"visualization/manage.py migrate
+  python "$PROJECT_DEST"visualization/manage.py collectstatic
 
   # add the cron task data
   #python manage.py crontab add
